854a77f5a1c801cb7c05e6a6294834120dc38090
Stenalo <60882835+Stenalo@users.noreply.github.com>
2024-11-08 11:07:04 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -137,7 +137,7 @@ return [
 
         'cache' => [
             'url' => env('REDIS_URL'),
-            'scheme' => 'tls,
+            'scheme' => 'tls',
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD', null),
